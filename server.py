66e60893a9221ea726975bc06c419d1166af8e6e
Evgeny Evseev <pelid80@gmail.com>
2019-04-15 17:22:04 +0300

    adds handler for archive creation

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from aiohttp import web
 import aiofiles
 
 
+async def archivate(request):
+    raise NotImplementedError
+
+
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
         index_contents = await index_file.read()
@@ -12,5 +16,6 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archivate),
     ])
     web.run_app(app)
