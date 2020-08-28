147cb1e8fc8b22823e4c130dfe5d168420361224
dkuba <dkubarev88@gmail.com>
2020-08-28 18:07:39 +0300

    downloading archived photos by parts is implemented

--- server.py
+++ server.py
@@ -1,9 +1,39 @@
+import asyncio
 from aiohttp import web
 import aiofiles
+import os
+
+INTERVAL_SECS = 1
+EMPTY_ARCHIVE_NAME = "empty_archive_name"
 
 
 async def archivate(request):
-    raise NotImplementedError
+
+    dir_name = request.match_info.get('archive_hash', "empty_archive_name")
+    if dir_name == EMPTY_ARCHIVE_NAME:
+        raise web.HTTPFound('')
+
+    response = web.StreamResponse()
+
+    response.headers['Content-Type'] = f'attachment; filename="{dir_name}.zip"'
+    await response.prepare(request)
+
+    cmd = f"cd test_photos;zip - {dir_name} -r"
+    print(cmd)
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    size = 0
+    while True:
+        chunk = await proc.stdout.read(50)
+        if not chunk:
+            print(size)
+            break
+        size += len(chunk)
+        await response.write(chunk)
+
+    return response
 
 
 async def handle_index_page(request):
