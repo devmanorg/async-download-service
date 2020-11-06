354a9b9affa8aa84c14684dd89cf7bdcc1e164a1
dima <dkubarev88@gmail.com>
2020-11-07 01:19:54 +0300

    refactored due to last code review

--- config.py
+++
@@ -1,6 +0,0 @@
-DELAY_RESPONSE = 1
-EMPTY_ARCHIVE_NAME = "empty_archive_name"
-PHOTOS_DIR = 'photos'
-PHOTOS_PATH = ''
-
-arg_config = {PHOTOS_DIR: '', DELAY_RESPONSE: True}
--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import asyncio
+import sys
+
 from aiohttp import web
 import aiofiles
 import logging
 import os
 import argparse
-from config import *
+import const
 
 
 logger = logging.getLogger(__file__)
+
+
+def err_print(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 async def stop_archivate(proc):
@@ -21,17 +27,16 @@ async def stop_archivate(proc):
 async def archivate(request):
     response = web.StreamResponse()
 
-    if os.path.exists(arg_config[PHOTOS_DIR]):
-        photo_dir_path = arg_config[PHOTOS_DIR]
-        dir_name_requested = os.path.split(photo_dir_path)[-1]
-    else:
-        dir_name_requested = request.match_info.get('archive_hash', EMPTY_ARCHIVE_NAME)
-        photo_dir_path = f'./photos/{dir_name_requested}'
+    photo_dir_path = app[const.PHOTOS_DIR]
 
-        dir_list = [dir_name for dir_name in os.listdir(PHOTOS_PATH) if os.path.isdir(os.path.join(PHOTOS_PATH, dir_name))]
+    try:
+        dir_name_requested = request.match_info.get('archive_hash')
+    except TypeError:
+        raise web.HTTPNotFound(text="Отсутствует запрос")
 
-    if dir_name_requested == EMPTY_ARCHIVE_NAME or dir_name_requested not in dir_list \
-            or not os.path.exists(photo_dir_path):
+    photo_dir_path = os.path.join(photo_dir_path, dir_name_requested)
+
+    if not os.path.exists(photo_dir_path):
         raise web.HTTPNotFound(text="Запрашиваемый архив не существует или удален")
 
     response.headers['Content-Type'] = f'attachment; filename="{dir_name_requested}.zip"'
@@ -51,15 +56,8 @@ async def archivate(request):
 
             await response.write(chunk)
 
-            if arg_config[DELAY_RESPONSE]:
-                await asyncio.sleep(arg_config[DELAY_RESPONSE])
-
-    except asyncio.CancelledError:
-        raise
-    except Exception:
-        raise
-    except SystemExit:
-        raise
+            if app[const.RESPONSE_DELAY]:
+                await asyncio.sleep(app[const.RESPONSE_DELAY])
     finally:
         await stop_archivate(proc)
 
@@ -73,31 +71,33 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archivate),
+    ])
+    
     parser = argparse.ArgumentParser(description='Async download file service')
-    parser.add_argument('--logging', type=bool, default=False, help='Logging enable/disable')
+    parser.add_argument('--logging_disable', default=True, help='Logging disable',
+                        action='store_false', dest='logging')
     parser.add_argument('--resp_delay', type=int, default=0, help='Response delay in sec', dest='delay')
     parser.add_argument('--photo_dir', type=str, default='', help='Photo directory path')
     args = parser.parse_args()
 
-    arg_config[DELAY_RESPONSE] = args.delay
-    arg_config[PHOTOS_DIR] = args.photo_dir
-
-    print('arg_config =', arg_config)
-
-    if os.path.exists(args.photo_dir):
-        PHOTOS_PATH = args.photo_dir
-    else:
-        PHOTOS_PATH = os.path.join(os.getcwd(), PHOTOS_DIR)
+    app[const.RESPONSE_DELAY] = args.delay
+    app[const.PHOTOS_DIR] = args.photo_dir
 
     if args.logging:
         logging.basicConfig(level=logging.INFO)
     else:
         logging.basicConfig(level=logging.WARNING)
 
-    app = web.Application()
-    app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
-    ])
+    if os.path.exists(args.photo_dir):
+        PHOTOS_PATH = args.photo_dir
+    else:
+        err_print("Несуществующий каталог файлов")
+        logger.error("Задана не существующая папка")
+        sys.exit()
+
     web.run_app(app, port=8080)
 
