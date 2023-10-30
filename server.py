8dab9d0aa9ba2a86983e30ae167bfba874a09fdd
vl.pushkarev <vl.pushkarev@samolet.ru>
2023-10-30 17:54:25 +0300

    step 10. logging.

--- server.py
+++ server.py
@@ -1,21 +1,60 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+import logging
+import os
+
+INTERVAL_SECS = 1
+CHUNK_SIZE = 100000
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 async def archive(request):
-    raise NotImplementedError
+    archive_hash = request.match_info.get("archive_hash", "Anonymous")
+    cwd = os.getcwd()
+    archive_path = f"{cwd}/test_photos/{archive_hash}/"
+    if not os.path.exists(archive_path):
+        raise web.HTTPNotFound()
+    response = web.StreamResponse()
+    response.headers["Content-Type"] = "application/octet-stream"
+    response.headers["Content-Disposition"] = "attachment; filename=photos.zip"
+    await response.prepare(request)
+
+    proc = await asyncio.create_subprocess_exec(
+        "zip",
+        "-r",
+        "-",
+        f".",
+        "*",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=archive_path,
+    )
+    chunk_number = 0
+    while True:
+        logger.info(f"Sending archive chunk {chunk_number}")
+        await response.write(await proc.stdout.read(n=CHUNK_SIZE))
+        chunk_number += 1
+        if proc.stdout.at_eof():
+            logger.info(f"Complete")
+            break
+    return response
 
 
 async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
+    async with aiofiles.open("index.html", mode="r") as index_file:
         index_contents = await index_file.read()
-    return web.Response(text=index_contents, content_type='text/html')
+    return web.Response(text=index_contents, content_type="text/html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = web.Application()
-    app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
-    ])
+    app.add_routes(
+        [
+            web.get("/", handle_index_page),
+            web.get("/archive/{archive_hash}/", archive),
+        ]
+    )
     web.run_app(app)
